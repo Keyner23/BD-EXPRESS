d348cc4da52faec6b12e0c3e39c484255a6ecc3e
keyner barrios <keinerba.ochoa@gmail.com>
2025-08-02 09:02:52 -0500

    feat:wip

--- src/conexion.js
+++ src/conexion.js
@@ -1,29 +1,37 @@
 import mysql from "mysql2"
 import express from "express"
 
-const conexion=mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    user:"root",
-    password:"TuNuevaContraseñaSegura",
-    database:"bd_riwi"
+const conexion = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "TuNuevaContraseñaSegura",
+    database: "bd_riwi"
 })
 
-const app=express()
+const app = express()
 
 
-app.get("/clientes",(req,resp)=>{
+app.get("/clientes", (req, resp) => {
     resp.json({
-        nombre:"keyner barrios"
+        nombre: "keyner",
+        apellido:"barrios"
     })
 })
 
-app.listen(3000,(error)=>{
-    if (error) throw error
-    console.log("api se levanto correctamente")
-})
+app.listen(3000, (error) => {
+    if (error) {
+        console.error("Error al iniciar el servidor:", error.message);
+        return;
+    }
+    console.log("api se levanto correctamente");
+});
 
-conexion.connect((error)=>{
-    if(error) throw error
-    console.log("se conecto con exito")
-})
+
+conexion.connect((error) => {
+    if (error) {
+        console.error("Error al conectar con MySQL:", error.message);
+        return;
+    }
+    console.log("se conecto con exito");
+});
