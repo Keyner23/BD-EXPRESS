73804fa88dcbcebff2627a3b248750d1d8e57f56
keyner barrios <keinerba.ochoa@gmail.com>
2025-08-02 10:15:52 -0500

    feat:wip

--- src/conexion.js
+++ src/conexion.js
@@ -1,5 +1,9 @@
 import mysql from "mysql2"
 import express from "express"
+import cors from "cors"
+
+// aqui creamos la conexion con la base de datos con el motor mysql con sus respectivos 
+// parametros dependiendo de tu ordenador
 
 const conexion = mysql.createConnection({
     host: "localhost",
@@ -9,16 +13,43 @@ const conexion = mysql.createConnection({
     database: "bd_riwi"
 })
 
+// aqui traemos los frameworks y librerias para poder manejarlos en el navegador
+// y para levantar el servidor con express
+
 const app = express()
 
+// este es para que ingrese por cualquier parte del navegador y no tenga conflicto
+app.use(cors())
+
+
+
+
+
+// aqui comenzamos a crear los endpoinst pero con express para ir a consular a la bd 
+// y realizar la consulta requerida
 
 app.get("/clientes", (req, resp) => {
-    resp.json({
-        nombre: "keyner",
-        apellido:"barrios"
+    conexion.query("select * from clientes", (error, result) => {
+        if (error) throw error
+
+        resp.json(result)
     })
 })
 
+app.get("/cuentas", (req, resp) => {
+    conexion.query("select * from cuentas", (error, result) => {
+        if (error) throw error
+
+        resp.json(result)
+    })
+})
+
+
+
+
+
+// aqui usamos un metodo de express para levantar la api en el puerto que deseamos
+// y hacemos una sencilla validacion
 app.listen(3000, (error) => {
     if (error) {
         console.error("Error al iniciar el servidor:", error.message);
@@ -27,7 +58,7 @@ app.listen(3000, (error) => {
     console.log("api se levanto correctamente");
 });
 
-
+// para verificar si la conexion es correcta con la bd
 conexion.connect((error) => {
     if (error) {
         console.error("Error al conectar con MySQL:", error.message);
@@ -35,3 +66,6 @@ conexion.connect((error) => {
     }
     console.log("se conecto con exito");
 });
+
+
+// podemos hacerlo de forma modular, pero sera mas adelante, SIGAN VIENDO.
--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ const $form = document.getElementById("miFormulario");
 
 $form.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  const $email = document.getElementById("exampleInputEmail1").value
+  const $contraseña = document.getElementById("exampleInputPassword1").value
+
+  fetch("http://localhost:3000/cuentas")
+    .then(resp => resp.json())
+    .then(user => {
+      user.forEach(user => {
+        if (user.email === $email && user.contraseña === $contraseña) {
+          alert("credenciales correctas.")
+        } else {
+          alert("credenciales incorrectas.")
+        }
+      });
+    })
 });
